2194aca0a3a58277c4410dbce7531c8d6ce6526c
joguit <jordiazur@gmail.com>
2017-12-08 12:33:44 +0100

    Update nn_calcula_pesos.py
    cambio de nombre sigmoid_derivada

--- nn_calcula_pesos.py
+++ nn_calcula_pesos.py
@@ -8,7 +8,7 @@ X = np.array([[0,0], [0,1], [1,0], [1,1]])      # Buttons states array
 Y = np.array([ [0],   [1],   [1],   [0]])       # LED states array
  
 def sigmoid (x): return 1/(1 + np.exp(-x))      # activation function
-def sigmoid_deriv(x):return x * (1 - x) 
+def sigmoid_derivada(x):return x * (1 - x) 
                                                 # weights on layer inputs
 Wh = np.random.uniform(size=(inputLayerSize, hiddenLayerSize))
 Wz = np.random.uniform(size=(hiddenLayerSize,outputLayerSize))
@@ -20,7 +20,7 @@ for i in range(epochs):
     E = Y - Z                                   # calculate error
     dZ = E * L                                  # delta Z
     Wz +=  H.T.dot(dZ)                          # calculate backpropagation part
-    dH = dZ.dot(Wz.T) * sigmoid_deriv(H)        # 
+    dH = dZ.dot(Wz.T) * sigmoid_derivada(H)        # 
     Wh +=  X.T.dot(dH)                          # update hidden layer weights
 
 print("**************** error ****************") 
